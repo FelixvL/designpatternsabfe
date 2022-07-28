6650ce2e133891b5538fe662d34bfb82efa904eb
Taapke <taapkebergsma@gmail.com>
2022-07-28 13:44:34 +0200

    nieuwe update featureTaapke

--- strategy/controller/StrategyLauncher.java
+++ strategy/controller/StrategyLauncher.java
@@ -1,9 +1,8 @@
 package designpatternsabfe.strategy.controller;
 
-import designpatternsabfe.strategy.model.Huisje;
-import designpatternsabfe.strategy.model.Schoonmaak;
-import designpatternsabfe.strategy.model.Seizoen;
-import designpatternsabfe.strategy.model.Situatie;
+import designpatternsabfe.strategy.model.*;
+
+import java.util.ArrayList;
 
 /**
  * @author Taapke Bergsma <t.bergsma@st.hanze.nl>
@@ -13,13 +12,44 @@ public class StrategyLauncher {
     public static void main(String[] args) {
         System.out.println("Dit programma is geschreven door Taapke");
 
-        Seizoen hoogseizoen = new Seizoen("hoogseizoen", 1.5);
+        // Huisjes
         Huisje goedkoopHuisje = new Huisje(2000, 20, 40);
+        Huisje normaalHuisje = new Huisje(4000, 45, 60);
+        Huisje duurHuisje = new Huisje(10000, 150, 100);
+
+        ArrayList<Huisje> huisjes = new ArrayList<>();
+        huisjes.add(goedkoopHuisje);
+        huisjes.add(normaalHuisje);
+        huisjes.add(duurHuisje);
+
+        // Seizoenen
+        Seizoen hoogseizoen = new Seizoen("hoogseizoen", 1.5);
+        Seizoen laagseizoen = new Seizoen("laagseizoen", 0.5);
+
+        // -- Schoonmaken in hoogseizoen
         Situatie situatie = new Situatie(new Schoonmaak());
-        double prijsSchoonmaakGoedkoopHuisje = situatie.voerHandelingUit(hoogseizoen, goedkoopHuisje);
+        for (Huisje huisje : huisjes) {
+            double schoonmaakPrijs = situatie.voerHandelingUit(hoogseizoen, huisje);
+            System.out.printf("De prijs om dit huis schoon te maken in het %s is %.2f\n",
+                    hoogseizoen.getNaam(), schoonmaakPrijs);
+        }
+        System.out.println();
 
-        System.out.printf("De prijs om dit huis schoon te maken in het %s is %.2f",
-                hoogseizoen.getNaam(), prijsSchoonmaakGoedkoopHuisje);
+        // -- Verkopen in hoogseizoen
+        situatie = new Situatie(new Verkoop());
+        for (Huisje huisje : huisjes) {
+            double verkoopPrijs = situatie.voerHandelingUit(hoogseizoen, huisje);
+            System.out.printf("De prijs om dit huis te verkopen in het %s is %.2f\n",
+                    hoogseizoen.getNaam(), verkoopPrijs);
+        }
+        System.out.println();
 
+        // -- Verhuren in laagseizoen
+        situatie = new Situatie(new Verhuur());
+        for (Huisje huisje : huisjes) {
+            double verkoopPrijs = situatie.voerHandelingUit(laagseizoen, huisje);
+            System.out.printf("De prijs om dit huis te verhuren in het %s is %.2f\n",
+                    hoogseizoen.getNaam(), verkoopPrijs);
+        }
     }
 }
