cf125cd5dacbb46fab0e8cdbc5cb119a5ba76d35
Emiel <emiel92@gmail.com>
2022-07-28 11:13:16 +0200

    check

---
+++ strategyemiel/StrategyLauncher.java
@@ -0,0 +1,12 @@
+package designpatternsabfe.strategyemiel;
+
+/**
+ * @author Emiel Bloem
+ * <p>
+ * Dit doet het programma
+ */
+public class StrategyLauncher {
+    public static void main(String[] args) {
+        System.out.println("van Emiel");
+    }
+}
