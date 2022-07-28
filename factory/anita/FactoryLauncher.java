b02579214661397d7e15b57cb525f6ab195ee7cb
Anita <anita.bos@hotmail.com>
2022-07-28 11:11:24 +0200

    test

---
+++ factory/anita/FactoryLauncher.java
@@ -0,0 +1,13 @@
+package designpatternsabfe.factory.anita;
+
+/**
+ * @Auteur Anita Bos <at.bos@st.hanze.nl>
+ * <p>
+ * Beschrijving doel code.
+ */
+
+public class FactoryLauncher {
+    public static void main(String[] args) {
+        System.out.println("van Anita");
+    }
+}
