e4bb4b171464d077708430319e3d224471d72080
FelixvL <digitaleservice@gmail.com>
2022-07-28 10:50:50 +0200

    go

---
+++ TestBestand.java
@@ -0,0 +1,5 @@
+package designpatternsabfe;
+
+public class TestBestand {
+
+}
---
+++ felix/Demo.java
@@ -0,0 +1,7 @@
+package designpatternsabfe.felix;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("go");
+	}
+}
