d46efcf98bc4f5e7eeefb3fdc3b6143d0b7d68b6
Alex Kuiper <kuiperalexander@gmail.com>
2022-07-28 11:11:27 +0200

    changes Alex

---
+++ mvcalex/MvcLauncher.java
@@ -0,0 +1,12 @@
+package designpatternsabfe.mvcalex;
+
+/**
+ * @author Alex Kuiper <al.kuiper@st.hanze.nl>
+ * <p>
+ * {wat doet dit?}
+ */
+public class MvcLauncher {
+    public static void main(String[] args) {
+        System.out.println("Alex");
+    }
+}
