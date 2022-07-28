55b4bbf3661cbd2407ce88421a9a9248da42d371
Thijs <jankeesendonald@hotmail.com>
2022-07-28 11:11:29 +0200

    changes Thijs

---
+++ factorythijs/FactoryLauncher.java
@@ -0,0 +1,13 @@
+package designpatternsabfe.factorythijs;
+
+/**
+ * @author Thijs Hettinga
+ * <p>
+ * Uitleg wat het doet
+ */
+
+public class FactoryLauncher {
+    public static void main(String[] args) {
+        System.out.println("van Thijs");
+    }
+}
